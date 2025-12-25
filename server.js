32116d72760761ae9e21fca1be8491feec7dc3f1
Philip Alegret <philip.alegret@gmail.com>
2025-12-25 08:55:03 -0500

    fix: correct middleware order

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const app = express();
 // Set static folder
 app.use(express.static('public'));
 
-let currentPrice = 60;
+// Parse URL-encoded HTML form bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Parse JSON bodies typically sent by APIs
+app.use(express.json());
 
 // Handle POST request for contacts search
 app.post('/search', async (req, res) => {
-  const searchTerm = req.body.search.toLowerCase();
+  const searchTerm = req?.body?.search?.toLowerCase();
 
   if (!searchTerm) {
     return res.send('<tr></tr>');
@@ -34,16 +38,7 @@ app.post('/search', async (req, res) => {
   res.send(searchResultHtml);
 });
 
-// The following two statements are middleware. They are used for getting raw
-// data from JSON API clients and form bodies.
-
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({ extended: true }));
-
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-
 // Start the server
-app.listen (3000, () => {
-  console.log('Server listening on port 3000...');
+app.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000...');
 });
